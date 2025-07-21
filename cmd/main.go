630ff4707f8789710f084fedaa59a82a079a691f
Black-tag <unnianandunni007@gmail.com>
2025-07-21 17:00:30 +0000

    postgres,goose,sqlc setup completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/blacktag/bugby-Go/internal/database"
+	"github.com/joho/godotenv"
+	"github.com/lib/pq"
+	
+)
+
+type apiConfig struct {
+	db *database.Queries
+
+} 
+
+
+
+func main() {
+
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgre", dbURL)
+	dbQueries := database.New(db)
+
+	cfg := apiapiConfig{
+		db: dbQueries,
+	}
+
+
+
+	mainFileServer := http.FileServer((http.Dir(".")))
+	mux := http.NewServeMux()
+	mux.Handle("/", mainFileServer)
+
+	
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("🌐 starting the server on: http://localhost:8080...")
+	err = server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Server failed: %v\n", err)
+	}
+
+
+
+
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module bugby
+module github.com/blacktag/bugby-Go
 
 go 1.24.0
+
+replace github.com/golang-jwt/jwt/v5 => github.com/golang-jwt/jwt/v5 v5.2.0
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
