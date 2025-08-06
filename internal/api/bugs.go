6010fd1ee1309810ba500af785183656ca965b59
Black-tag <unnianandunni007@gmail.com>
2025-08-06 17:28:53 +0000

    added unit test for bug handlers

--- internal/api/bugs.go
+++ internal/api/bugs.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/blacktag/bugby-Go/internal/database"
 	"github.com/blacktag/bugby-Go/internal/utils"
+	
+
+	// "github.com/casbin/casbin/v2/log"
 	"github.com/google/uuid"
 )
 
@@ -52,7 +55,7 @@ func (cfg *APIConfig) CreateBugHandler (w http.ResponseWriter, r *http.Request){
 	slog.Info("handler entered")
 	logger := slog.Default().With(
 		"handler", "CreateBugHandler",
-		"methid", r.Method,
+		"method", r.Method,
 		"path", r.URL.Path,
 	)
 	w.Header().Set("Content-Type", "application/json")
@@ -128,23 +131,36 @@ func (cfg *APIConfig) GetBugsHandler (w http.ResponseWriter, r *http.Request) {
 // @Router /bugs/{bugid} [get]
 // @Security BearerAuth
 func (cfg *APIConfig) GetBugByIDHandler (w http.ResponseWriter, r *http.Request) {
-	
+	slog.Info("Handler started")
+	logger := slog.Default().With(
+		"handler", "CreateBugHandler",
+		"methid", r.Method,
+		"path", r.URL.Path,
+	)
 	bugIDParam := r.PathValue("bugid")
+	
 
 	if bugIDParam == "" {
+		logger.Info("no id given by user")
 		utils.RespondWithError(w, http.StatusBadRequest, " no id given ")
 		return
 	}
 	bugID, err := uuid.Parse(bugIDParam)
 	if err != nil {
+		logger.Error("invalid id format", "error", err)
 		utils.RespondWithError(w, http.StatusBadRequest, "wrong Id format ")
 		return
 	}
+	logger.Info("parsed uuid", "bugID", bugID)
+
+	logger.Info("calling database")
 	bug, err := cfg.DB.GetBugsByID(r.Context(), bugID)
 	if err != nil {
+		logger.Error("database error", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, " bug not found ")
 		return
 	}
+	logger.Info("response ready", "bug", bug)
 	utils.RespondWithJSON(w, http.StatusOK, bug)
 }
 
@@ -161,37 +177,55 @@ func (cfg *APIConfig) GetBugByIDHandler (w http.ResponseWriter, r *http.Request)
 // @Failure 500 {object} utils.ErrorResponse "Internal Server Error"
 // @Router /bug/{bugid} [put]
 // @Security BearerAuth
-func (cfg *APIConfig) UpadteBugHandler (w http.ResponseWriter, r *http.Request) {
+func (cfg *APIConfig) UpdateBugHandler (w http.ResponseWriter, r *http.Request) {
+	logger := slog.With(
+		"handler", "UpdateBugHandler",
+		"method", r.Method, 
+		"path", r.URL.Path,
+	)
+	logger.Info("Entered handler")
 	userIDVal := r.Context().Value("userID")
 	userID, ok := userIDVal.(uuid.UUID)
+	logger = logger.With("userID", userID)
 	if !ok {
+		logger.Error(" user id not given or invalid")
 		utils.RespondWithError(w, http.StatusUnauthorized, "invalid or missing user ID")
 		return
 	}
 	bugParam := r.PathValue("bugid")
+	
 	if bugParam == "" {
-		utils.RespondWithError(w, http.StatusBadRequest, " no id given ")
+		logger.Info("no bugID in request")
+		utils.RespondWithError(w, http.StatusBadRequest, " no bugID given ")
 		return
 	}
 	bugID, err := uuid.Parse(bugParam)
 	if err != nil {
+		logger.Error("given id format is wrong", "error", err)
 		utils.RespondWithError(w, http.StatusBadRequest, "wrong format Id")
 		return
 	}
+	logger = logger.With("bugId", bugID)
 	
 	var req UpdateBugRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		logger.Error("given request body in wrong format", "error", err)
 		utils.RespondWithError(w, http.StatusBadRequest, "invalid request body")
 		return
 	}
 	
+	
 	bug, err := cfg.DB.GetBugsByID(r.Context(), bugID)
+	logger.Info("doing database operation")
 	if err != nil { 
+		logger.Error("databse error bug not found in database", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "no bug  found with the id")
 		return
 
 	}
+	logger = logger.With("bug", bug)
 	if userID != bug.PostedBy {
+		logger.Error("unauthorised to edit the bug, not owned by user", "error", err)
 		utils.RespondWithError(w, http.StatusUnauthorized, "only author can delete the bug")
 		return
 
@@ -202,9 +236,12 @@ func (cfg *APIConfig) UpadteBugHandler (w http.ResponseWriter, r *http.Request) 
 		Description: toNullString(req.Description),
 		
 	}
+	logger = logger.With("params", params)
 	
 	err = cfg.DB.UpdateBugByID(r.Context(), params)
+	logger.Info("doing database updation")
 	if err != nil {
+		logger.Error("updating bug in databse failed", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot update bug")
 		return
 	}
@@ -213,7 +250,10 @@ func (cfg *APIConfig) UpadteBugHandler (w http.ResponseWriter, r *http.Request) 
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot fetch updated bug")
 		return
 	}
+	logger = logger.With("updatedbug", updatedbug)
+
 	utils.RespondWithJSON(w, http.StatusOK, updatedbug)
+	logger.Info("completed updation")
 }
 func toNullString(s *string) sql.NullString {
 	if s == nil {
@@ -235,50 +275,69 @@ func toNullString(s *string) sql.NullString {
 // @Router /bugs/{bugid} [delete]
 // @Security BearerAuth
 func (cfg *APIConfig) DeleteBugByIDHandler (w http.ResponseWriter, r *http.Request) {
+	logger := slog.Default().With(
+		"handler", "DeleteBugByIDHandler",
+		"method", r.Method,
+		"path", r.URL.Path,
+	)
+	logger.Info("Entered the deleteHandler")
 	userIDVal := r.Context().Value("userID")
 	userID, ok := userIDVal.(uuid.UUID)
 	if !ok {
 		utils.RespondWithError(w, http.StatusUnauthorized, "invalid or missing user ID")
 		return
 	}
+	logger = logger.With("userID", userID)
 	userVal := r.Context().Value("user")
 	user, ok := userVal.(database.User)
 	if !ok {
+		logger.Error("user data not found in contest")
     	utils.RespondWithError(w, http.StatusUnauthorized, "user not in context")
     	return
 	}
 
 	if user.Role != "admin" {
+		logger.Error("user has no admin status, cannot delete")
     	utils.RespondWithError(w, http.StatusForbidden, "admin access required")
     	return
 	}
 
 	bugParam := r.PathValue("bugid")
 	if bugParam == "" {
+		logger.Error("bug id not found in path")
 		utils.RespondWithError(w, http.StatusBadRequest, "no id in the request")
 		return
 	}
 	bugID, err := uuid.Parse(bugParam)
 	if err != nil {
+		logger.Error("bugID in wrong format", "error", err)
 		utils.RespondWithError(w, http.StatusBadRequest, "wrong format id")
 		return
 	}
+	logger = logger.With("bugId", bugID)
+	logger.Info("started querying to get existing bug with bugID")
 	bug, err := cfg.DB.GetBugsByID(r.Context(), bugID)
 	if err != nil { 
+		logger.Error("database operation failed", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "no bug  found with the id")
 		return
 
 	}
+	logger = logger.With("bug", bug)
 	if userID != bug.PostedBy {
+		logger.Error("user is not the author of the bug")
 		utils.RespondWithError(w, http.StatusUnauthorized, "only author can delete the bug")
 		return
 
 	}
+	logger.Info("starting databse operationto delete bug")
 	err = cfg.DB.DeleteBugByID(r.Context(), bugID)
 	if err != nil {
+		logger.Error("databse operation failed, cannot delete", "error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot delete bug")
 		return
 	}
+	logger.Info("completed handler ")
 	w.WriteHeader(http.StatusNoContent)
 }
 
