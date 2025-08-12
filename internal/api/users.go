284001af58337ccbd6ca396b5c31f48724e4d734
Black-tag <unnianandunni007@gmail.com>
2025-08-12 07:15:00 +0000

    added new handler for getting users

--- internal/api/users.go
+++ internal/api/users.go
@@ -3,7 +3,7 @@ package api
 import (
 	"database/sql"
 	"encoding/json"
-	
+	"log/slog"
 
 	"log"
 	"net/http"
@@ -13,25 +13,22 @@ import (
 	"github.com/blacktag/bugby-Go/internal/utils"
 	"github.com/google/uuid"
 )
+
 type CreateUserRequest struct {
-		Email string `json:"email" example:"user@example.com"`
-		Password string `json:"password" example:"mysecret"`
-		
-		
+	Email    string `json:"email" example:"user@example.com"`
+	Password string `json:"password" example:"mysecret"`
 }
 
 type CreateUserResponse struct {
-        ID        uuid.UUID `json:"id"`
-        Email     string    `json:"email"`
-        CreatedAt time.Time `json:"created_at"`
-        UpdatedAt time.Time `json:"updated_at"`
-		
-    }
+	ID        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 type LoginUserRequest struct {
 	Email    string `json:"email" example:"user@example.com"`
 	Password string `json:"password" example:"mysecret"`
-		
 }
 type LoginResponse struct {
 	ID           uuid.UUID `json:"id"`
@@ -47,12 +44,11 @@ type UpdateRequest struct {
 }
 
 type UpdateResponse struct {
-	ID uuid.UUID `json:"id"`
-	Email string `json:"email"`
+	ID        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
-
 
 // @Summary Creates a new  user
 // @Description Creates user with Email and Password
@@ -66,43 +62,54 @@ type UpdateResponse struct {
 // @Router /users [post]
 // @Security BearerAuth
 func (cfg *APIConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
-	
+	logger := slog.Default().With(
+		"handler", "CreateUSerHandler", 
+		"method", r.Method, 
+		"path", r.URL.Path,
+	)
+	logger.Info("enetered Hnadler")
 	w.Header().Set("Content-Type", "application/json")
 	var req CreateUserRequest
-	 err := json.NewDecoder(r.Body).Decode(&req)
-	 if err != nil {
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
 		utils.RespondWithError(w, http.StatusBadRequest, "invalid request body")
 		return
-	 }
+	}
 
-	 if req.Email == "" {
+	if req.Email == "" {
 		utils.RespondWithError(w, http.StatusBadRequest, "email field required")
 		return
-	 }
-	 
-	 hashed_password, err := utils.HashPassword(req.Password)
-	 if err != nil {
+	}
+
+	hashed_password, err := utils.HashPassword(req.Password)
+	if err != nil {
+		logger.Error("hashing failed", "Error", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot hashPassword")
 		return
-	 }
-	 
-	 user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		Email: req.Email,
+	}
+	logger = logger.With("hashed_password in handler", hashed_password)
+	logger.Info("starting database operation")
+	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		Email:          req.Email,
 		HashedPassword: hashed_password,
-	 })
-	 if err != nil {
+	})
+	if err != nil {
+		logger.Error("database opertaion failed", "error", err)
 		log.Printf("cannot create user: %v", err)
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot create user")
 		return
-	 }
-	 utils.RespondWithJSON(w, http.StatusCreated, CreateUserResponse{
-		ID: user.ID,
+	}
+	logger = logger.With("user", user)
+	utils.RespondWithJSON(w, http.StatusCreated, CreateUserResponse{
+		ID:        user.ID,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
-		Email: user.Email,
-	 })
+		Email:     user.Email,
+	})
+	logger.Info("handler function completed")
 
 }
+
 // @Summary Login an existing  user
 // @Description Existing users can login using email and password
 // @Tags users
@@ -111,12 +118,12 @@ func (cfg *APIConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request) 
 // @Param request body LoginUserRequest true "user login data"
 // @Success 200 {object} LoginResponse
 // @Failure 400 {object} utils.ErrorResponse "Bad Request - Invalid input"
-// @Failure 401 {object} utils.ErrorResponse "Invalid credentials" 
+// @Failure 401 {object} utils.ErrorResponse "Invalid credentials"
 // @Failure 500 {object} utils.ErrorResponse "Internal Server Error"
 // @Router /login [post]
 // @Security BearerAuth
 func (cfg *APIConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
-	
+
 	var req LoginUserRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -130,19 +137,16 @@ func (cfg *APIConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
-
-	err = utils.CheckPasswordAndHash(req.Password, user.HashedPassword )
+	err = utils.CheckPasswordAndHash(req.Password, user.HashedPassword)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusUnauthorized, "incorrect email or password")
 		return
 	}
-	
-	token, err := utils.MakeJWT(user.ID, cfg.SECRET,time.Hour)
+
+	token, err := utils.MakeJWT(user.ID, cfg.SECRET, time.Hour)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot generate token")
 		return
-		
 
 	}
 	refreshToken, err := utils.MakeRefreshToken()
@@ -152,8 +156,8 @@ func (cfg *APIConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 	refreshExpiresAt := time.Now().Add(60 * 24 * time.Hour)
 
 	err = cfg.DB.CreateRefreshToken(r.Context(), database.CreateRefreshTokenParams{
-		Token: refreshToken,
-		UserID: user.ID,
+		Token:     refreshToken,
+		UserID:    user.ID,
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		ExpiresAt: refreshExpiresAt,
@@ -163,15 +167,15 @@ func (cfg *APIConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot store refreshed token")
 	}
 	utils.RespondWithJSON(w, http.StatusOK, LoginResponse{
-		ID: user.ID,
-		Email: user.Email,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Token: token,
+		ID:           user.ID,
+		Email:        user.Email,
+		CreatedAt:    user.CreatedAt,
+		UpdatedAt:    user.UpdatedAt,
+		Token:        token,
 		RefreshToken: refreshToken,
-
 	})
 }
+
 // @Summary Refresh jwtoken of an existing user
 // @Description Existing users can refresh jwt token for future use
 // @Tags refreshTokens
@@ -185,44 +189,44 @@ func (cfg *APIConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 // @Security BearerAuth
 func (cfg *APIConfig) RefreshTokenHandler(w http.ResponseWriter, r *http.Request) {
 	tokenString, err := utils.GetBearerToken(r.Header)
-	
+
 	if err != nil {
 		utils.RespondWithError(w, http.StatusUnauthorized, "missing refresh token")
 		return
 	}
-	refreshToken, err := cfg.DB.GetUserFromRefreshToken(r.Context(),tokenString)
+	refreshToken, err := cfg.DB.GetUserFromRefreshToken(r.Context(), tokenString)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusUnauthorized, "invalid token")
 		return
 	}
-	
-	if refreshToken.RevokedAt.Valid || time.Now().After(refreshToken.ExpiresAt){ 
+
+	if refreshToken.RevokedAt.Valid || time.Now().After(refreshToken.ExpiresAt) {
 		utils.RespondWithError(w, http.StatusUnauthorized, "refresh token expired or revoked")
 		return
 	}
 	newAccessToken, err := utils.MakeJWT(refreshToken.UserID, cfg.SECRET, time.Hour)
 	if err != nil {
-		utils.RespondWithError(w, http.StatusInternalServerError,"failed to create access token")
-		return 
+		utils.RespondWithError(w, http.StatusInternalServerError, "failed to create access token")
+		return
 	}
 	utils.RespondWithJSON(w, http.StatusOK, map[string]string{
 		"token": newAccessToken,
-	})	
-	
-	
+	})
+
 }
+
 // @Summary Revoke user token
 // @Description Existing users can revoke token using email and password
 // @Tags users
 // @Accept json
 // @Produce json
-// @Success 204 
+// @Success 204
 // @Failure 400 {object} utils.ErrorResponse "Bad Request - Invalid input"
 // @Failure 401 {object} utils.ErrorResponse "Unauthorized - Missing/invalid credentials"
 // @Failure 500 {object} utils.ErrorResponse "Internal Server Error"
 // @Router /revoke [post]
 // @Security BearerAuth
-func (cfg *APIConfig) RevokeTokenHandler (w http.ResponseWriter, r *http.Request) {
+func (cfg *APIConfig) RevokeTokenHandler(w http.ResponseWriter, r *http.Request) {
 	tokenString, ok := r.Context().Value("refreshTokenString").(string)
 	if !ok {
 		utils.RespondWithError(w, http.StatusUnauthorized, "invalid token string in context")
@@ -231,11 +235,10 @@ func (cfg *APIConfig) RevokeTokenHandler (w http.ResponseWriter, r *http.Request
 	err := cfg.DB.RevokeRefreshToken(r.Context(), tokenString)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "Failed to revoke token")
-        return
-    }
+		return
+	}
 	w.WriteHeader(http.StatusNoContent)
 }
-
 
 // @Summary Update an existing  user
 // @Description Existing users can update their info using email and password
@@ -250,7 +253,7 @@ func (cfg *APIConfig) RevokeTokenHandler (w http.ResponseWriter, r *http.Request
 // @Router /users [put]
 // @Security BearerAuth
 func (cfg *APIConfig) UpdateCredentialsHandler(w http.ResponseWriter, r *http.Request) {
-	
+
 	userIDVal := r.Context().Value("userID")
 	userID, ok := userIDVal.(uuid.UUID)
 	if !ok {
@@ -258,21 +261,20 @@ func (cfg *APIConfig) UpdateCredentialsHandler(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	
 	var req UpdateRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		utils.RespondWithError(w, http.StatusBadRequest, "invalid request body")
 		return
 	}
-	hashedpassword, err :=utils.HashPassword(req.Password)
+	hashedpassword, err := utils.HashPassword(req.Password)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "cannot hash the new password")
 		return
 	}
-	err = cfg.DB.UpdateUserCredentials(r.Context(),database.UpdateUserCredentialsParams{
-		Email: req.Email,
+	err = cfg.DB.UpdateUserCredentials(r.Context(), database.UpdateUserCredentialsParams{
+		Email:          req.Email,
 		HashedPassword: hashedpassword,
-		ID: userID,
+		ID:             userID,
 	})
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "failed to update user credentials")
@@ -281,21 +283,30 @@ func (cfg *APIConfig) UpdateCredentialsHandler(w http.ResponseWriter, r *http.Re
 	user, err := cfg.DB.GetUserByEmail(r.Context(), req.Email)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "failed to fetch the updated user")
-		return 
+		return
 	}
-	
+
 	utils.RespondWithJSON(w, http.StatusOK, UpdateResponse{
-		ID: user.ID,
-		Email: user.Email,
+		ID:        user.ID,
+		Email:     user.Email,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
-		
-		
 	})
 }
 
 
+func (cfg *APIConfig) GetUsersHandler (w http.ResponseWriter, r *http.Request) {
+	logger := slog.Default().With(
+		"handler", "GetUSerHandler",
+	)
+	logger.Info("entered handler")
+	user, err := cfg.DB.GetAllUsers(r.Context())
+	if err != nil {
+		logger.Error("databse operation(fetching users) failed: %v", err)
+		utils.RespondWithError(w, http.StatusInternalServerError, "cannot fetch users")
+	}
+	logger = logger.With("user list", user)
+	utils.RespondWithJSON(w, http.StatusOK, user)
+	logger.Info("completed handler(getall users)")
 
-
-
-
+}
