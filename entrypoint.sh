74c1c68bba6ceff49e2d57219213056e3662edcf
Black-tag <unnianandunni007@gmail.com>
2025-08-12 09:13:21 +0000

    changed entry.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-
+DB_URL=${DATABASE_URL:-"postgresql://postgres:postgres@db:5432/bugby?sslmode=disable"}
+echo "🔌 Using database URL: ${DB_URL}"
 echo "🔄 Waiting for Postgres to be ready..."
 until psql "postgresql://postgres:postgres@db:5432/bugby?sslmode=disable" -c '\q' > /dev/null 2>&1; do
   sleep 1
@@ -8,6 +9,9 @@ done
 
 echo "✅ Running migrations with Goose..."
 goose -dir ./internal/db/migrations postgres "postgres://postgres:postgres@db:5432/bugby?sslmode=disable" up
+
+echo "✅ Running migrations with Goose..."
+goose -dir ./internal/db/migrations postgres "${DB_URL}" up
 
 echo "🚀 Starting Go app..."
 ./bugby
