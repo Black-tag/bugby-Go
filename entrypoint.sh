7074c9ef31993d4336f0c595bb69bc6e1a21a660
Black-tag <unnianandunni007@gmail.com>
2025-09-03 07:01:56 +0000

    added new env

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 # DB_URL="${DB_URL}"
 echo "🔌 Using database URL: ${DB_URL}"
 echo "🔄 Waiting for Postgres to be ready..."
-until pg_isready "$DB_URL" -c '\q' > /dev/null 2>&1; do
+until psql "$DB_URL" -c '\q' > /dev/null 2>&1; do
   echo "postgres is unavailible sleepin...."
   sleep 1
 done
