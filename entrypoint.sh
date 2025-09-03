b3b3475927168695cd7df007f923f1328646f5d5
Black-tag <unnianandunni007@gmail.com>
2025-09-03 06:40:21 +0000

    changed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,16 @@ set -e
 
 
 
-DB_URL="${DB_URL}"
+# DB_URL="${DB_URL}"
 echo "🔌 Using database URL: ${DB_URL}"
 echo "🔄 Waiting for Postgres to be ready..."
-# until pg_isready "$DB_URL" -c '\q' > /dev/null 2>&1; do
-#   sleep 1
-# done
-until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
-  sleep 2
+until pg_isready "$DB_URL" -c '\q' > /dev/null 2>&1; do
+  echo "postgres is unavailible sleepin...."
+  sleep 1
 done
+# until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
+#   sleep 2
+# done
 
 echo "✅ Running migrations with Goose..."
 goose -dir ./internal/db/migrations postgres "$DB_URL" up
